957d00fa8aa7fe91d15f104c292759c09d07b937
Oegly <eva@dirtcrate.com>
2022-07-21 13:42:01 +0200

    Fix #1 Make controls more responsive

--- src/input.ts
+++ src/input.ts
@@ -1,21 +1,34 @@
 export default class Inputs {
   legal: string[] = ['w', 'a', 's', 'd', ' '];
   pressed: string[] = [];
-  
+  down: string[] = [];
+  up: string[] = [];
+
+  update() {
+    this.pressed = this.pressed
+    .filter((btn, i) => this.pressed.indexOf(btn) == i)
+    .concat(this.down)
+    // If key is pressed and released this tick, remain until next tick
+    .filter(btn => !this.up.includes(btn) || this.down.includes(btn));
+
+    // Keys pressed and removed this tick are to be removed next tick
+    this.up = this.up.filter(btn => this.down.includes(btn));
+    this.down = [];
+  }
+
   press(btn: string) {
-    if (this.pressed.includes(btn)) {
+    if (this.down.includes(btn) || !this.legal.includes(btn)) {
       return;
     }
-  
-    this.pressed.push(btn);
+
+    this.down.push(btn);
   }
   
   release(btn: string) {
-    let i = this.pressed.indexOf(btn);
-  
-    if (i > -1) {
-      this.pressed.splice(i, 1);
+    if (this.up.includes(btn)) {
+      return;
     }
+
+    this.up.push(btn);
   }
 }
-
